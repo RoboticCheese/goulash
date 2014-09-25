f221db7f80a58f480a0f2bd94ea5225ce80207aa
Jonathan Hartman <j@p4nt5.com>
2014-09-25 14:24:18 -0700

    Clean up test data into a map

--- cookbook/cookbook_test.go
+++ cookbook/cookbook_test.go
@@ -8,26 +8,22 @@ import (
 	"testing"
 )
 
-var name = "chef-dk"
-var maintainer = "roboticcheese"
-var description = "Installs/configures the Chef-DK"
-var category = "Other"
-var latest_version = "https://supermarket.getchef.com/api/v1/cookbooks/" +
-	"chef-dk/versions/2.0.1"
-var external_url = "https://github.com/RoboticCheese/chef-dk-chef"
-var average_rating = "null"
-var created_at = "2014-06-24T01:14:49.000Z"
-var updated_at = "2014-09-20T04:46:00.780Z"
-var deprecated = "false"
-var foodcritic_failure = "false"
-var versions = `
-	[
-		"https://supermarket.getchef.com/api/v1/cookbooks/chef-dk/versions/2.0.1",
-		"https://supermarket.getchef.com/api/v1/cookbooks/chef-dk/versions/2.0.0"
-	]
-`
-var metrics = `
-	{
+var test_data = map[string]string{
+	"name":               "chef-dk",
+	"maintainer":         "roboticcheese",
+	"description":        "Installs/configures the Chef-DK",
+	"category":           "Other",
+	"latest_version":     "https://supermarket.getchef.com/api/v1/cookbooks/chef-dk/versions/2.0.1",
+	"external_url":       "https://github.com/RoboticCheese/chef-dk-chef",
+	"average_rating":     "null",
+	"created_at":         "2014-06-24T01:14:49.000Z",
+	"updated_at":         "2014-09-20T04:46:00.780Z",
+	"deprecated":         "false",
+	"foodcritic_failure": "false",
+	"versions": `
+		[ "https://supermarket.getchef.com/api/v1/cookbooks/chef-dk/versions/2.0.1",
+		 "https://supermarket.getchef.com/api/v1/cookbooks/chef-dk/versions/2.0.0"]`,
+	"metrics": `{
 		"downloads": {
 			"total": 100,
 			"versions": {
@@ -36,23 +32,23 @@ var metrics = `
 			}
 		},
 		"followers": 20
-	}
-`
+	}`,
+}
 
 func jsonified() (res string) {
-	res = `{"name": "` + name + `",` +
-		`"maintainer": "` + maintainer + `",` +
-		`"description": "` + description + `",` +
-		`"category": "` + category + `",` +
-		`"latest_version": "` + latest_version + `",` +
-		`"external_url": "` + external_url + `",` +
-		`"average_rating": ` + average_rating + `,` +
-		`"created_at": "` + created_at + `",` +
-		`"updated_at": "` + updated_at + `",` +
-		`"deprecated": ` + deprecated + `,` +
-		`"foodcritic_failure": ` + foodcritic_failure + `,` +
-		`"versions": ` + versions + `,` +
-		`"metrics": ` + metrics + `}`
+	res = `{"name": "` + test_data["name"] + `",` +
+		`"maintainer": "` + test_data["maintainer"] + `",` +
+		`"description": "` + test_data["description"] + `",` +
+		`"category": "` + test_data["category"] + `",` +
+		`"latest_version": "` + test_data["latest_version"] + `",` +
+		`"external_url": "` + test_data["external_url"] + `",` +
+		`"average_rating": ` + test_data["average_rating"] + `,` +
+		`"created_at": "` + test_data["created_at"] + `",` +
+		`"updated_at": "` + test_data["updated_at"] + `",` +
+		`"deprecated": ` + test_data["deprecated"] + `,` +
+		`"foodcritic_failure": ` + test_data["foodcritic_failure"] + `,` +
+		`"versions": ` + test_data["versions"] + `,` +
+		`"metrics": ` + test_data["metrics"] + `}`
 	return
 }
 
@@ -80,16 +76,15 @@ func Test_New_1_NoError(t *testing.T) {
 		t.Fatalf("Expected no error, got: %v", err)
 	}
 	for k, v := range map[string]string{
-		c.Endpoint:    ts.URL + "/api/v1/cookbooks/chef-dk",
-		c.Name:        "chef-dk",
-		c.Maintainer:  "roboticcheese",
-		c.Description: "Installs/configures the Chef-DK",
-		c.Category:    "Other",
-		c.LatestVersion: "https://supermarket.getchef.com/api/v1/" +
-			"cookbooks/chef-dk/versions/2.0.1",
-		c.ExternalURL: "https://github.com/RoboticCheese/chef-dk-chef",
-		c.CreatedAt:   "2014-06-24T01:14:49.000Z",
-		c.UpdatedAt:   "2014-09-20T04:46:00.780Z",
+		c.Endpoint:      ts.URL + "/api/v1/cookbooks/chef-dk",
+		c.Name:          test_data["name"],
+		c.Maintainer:    test_data["maintainer"],
+		c.Description:   test_data["description"],
+		c.Category:      test_data["category"],
+		c.LatestVersion: test_data["latest_version"],
+		c.ExternalURL:   test_data["external_url"],
+		c.CreatedAt:     test_data["created_at"],
+		c.UpdatedAt:     test_data["updated_at"],
 	} {
 		if k != v {
 			t.Fatalf("Expected: %v, got: %v", v, k)
@@ -130,7 +125,7 @@ func Test_New_1_NoError(t *testing.T) {
 }
 
 func Test_New_2_NilFoodcriticFailure(t *testing.T) {
-	foodcritic_failure = "null"
+	test_data["foodcritic_failure"] = "null"
 	ts := start_http()
 	defer ts.Close()
 
@@ -148,7 +143,7 @@ func Test_New_2_NilFoodcriticFailure(t *testing.T) {
 }
 
 func Test_New_3_AverageRating(t *testing.T) {
-	average_rating = "20"
+	test_data["average_rating"] = "20"
 	ts := start_http()
 	defer ts.Close()
 
