adbb25463997c63a3eb480b81564a229b4d09be3
Jonathan Hartman <j@p4nt5.com>
2014-11-08 22:32:43 -0800

    Split Component into its own file

---
+++ common/component.go
@@ -0,0 +1,55 @@
+// Author:: Jonathan Hartman (<j@p4nt5.com>)
+//
+// Copyright (C) 2014, Jonathan Hartman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+Package goulash implements an API client for the Chef Supermarket.
+
+This file defines a common, shared Component struct.
+*/
+package common
+
+// Component defines variables to be shared by all the Goulash structs.
+type Component struct {
+	Endpoint string
+	ETag     string
+}
+
+func New(endpoint string) (c Component, err error) {
+	c = Component{}
+	c.Endpoint = endpoint
+	c.ETag, err = getETag(c.Endpoint)
+	return
+}
+
+// Empty checks whether a Component struct has been populated with anything
+// or still holds all the base defaults.
+func (c *Component) Empty() (empty bool) {
+	empty = Empty(c)
+	return
+}
+
+// Equals checks whether one Component struct is equal to another.
+func (c1 *Component) Equals(c2 Supermarketer) (equal bool) {
+	equal = Equals(c1, c2)
+	return
+}
+
+// Diff returns any attributes that have been changed from one Component struct
+// to another.
+func (c1 *Component) Diff(c2 Supermarketer) (pos, neg Supermarketer) {
+	pos, neg = Diff(c1, c2, &Component{}, &Component{})
+	return
+}
---
+++ common/component_test.go
@@ -0,0 +1,153 @@
+package common
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+var http_etag = ""
+var http_data = "SOME HTTP DATA"
+
+func start_http() (ts *httptest.Server) {
+	ts = httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				if http_etag != "" {
+					w.Header().Set("ETag", http_etag)
+				}
+				fmt.Fprint(w, http_data)
+			},
+		),
+	)
+	return
+}
+
+func Test_Component_1(t *testing.T) {
+	type Thing struct {
+		Component
+	}
+
+	res := Thing{
+		Component: Component{
+			Endpoint: "something",
+			ETag:     "anotherthing",
+		},
+	}
+	if res.Endpoint != "something" {
+		t.Fatalf("Expected 'something', got: %v", res.Endpoint)
+	}
+	if res.ETag != "anotherthing" {
+		t.Fatalf("Expected 'anotherthing', got: %v", res.ETag)
+	}
+}
+
+func Test_CNew_1_NoETag(t *testing.T) {
+	ts := start_http()
+	defer ts.Close()
+
+	c, err := New(ts.URL)
+	if err != nil {
+		t.Fatalf("Expected no err, got: %v", err)
+	}
+	if c.Endpoint != ts.URL {
+		t.Fatalf("Expected '%v', got: %v", ts.URL, c.Endpoint)
+	}
+	if c.ETag != "" {
+		t.Fatalf("Expected empty str, got: %v", c.ETag)
+	}
+}
+
+func Test_CNew_2_ETag(t *testing.T) {
+	http_etag = "hellothere"
+	ts := start_http()
+	defer ts.Close()
+
+	c, err := New(ts.URL)
+	if err != nil {
+		t.Fatalf("Expected no err, got: %v", err)
+	}
+	if c.Endpoint != ts.URL {
+		t.Fatalf("Expected '%v', got: %v", ts.URL, c.Endpoint)
+	}
+	if c.ETag != "hellothere" {
+		t.Fatalf("Expected 'hellothere', got: %v", c.ETag)
+	}
+}
+
+func Test_CEmpty_1_Empty(t *testing.T) {
+	c := new(Component)
+	res := c.Empty()
+	if res != true {
+		t.Fatalf("Expected true, got: %v", res)
+	}
+}
+
+func Test_CEmpty_2_HasEndpoint(t *testing.T) {
+	c := new(Component)
+	c.Endpoint = "https://example.com"
+	res := c.Empty()
+	if res != false {
+		t.Fatalf("Expected false, got: %v", res)
+	}
+}
+
+func Test_CEmpty_3_HasETag(t *testing.T) {
+	c := new(Component)
+	c.ETag = "thing"
+	res := c.Empty()
+	if res != false {
+		t.Fatalf("Expected false, got: %v", res)
+	}
+}
+
+func Test_CDiff_1_Equal(t *testing.T) {
+	c1 := Component{Endpoint: "abc", ETag: "def"}
+	c2 := Component{Endpoint: "abc", ETag: "def"}
+	pos1, neg1 := c1.Diff(&c2)
+	pos2, neg2 := c2.Diff(&c1)
+	for _, i := range []Supermarketer{pos1, neg1, pos2, neg2} {
+		if i != nil {
+			t.Fatalf("Expected nil, got: %v", i)
+		}
+	}
+}
+
+func Test_CDiff_2_AddedAndDeletedData(t *testing.T) {
+	c1 := Component{}
+	c2 := Component{Endpoint: "abc", ETag: "def"}
+	pos1, neg1 := c1.Diff(&c2)
+	pos2, neg2 := c2.Diff(&c1)
+	for _, i := range []Supermarketer{neg1, pos2} {
+		if i != nil {
+			t.Fatalf("Expected nil, got: %v", i)
+		}
+	}
+	for _, k := range [][]Supermarketer{
+		{pos1, &Component{Endpoint: "abc", ETag: "def"}},
+		{neg2, &Component{Endpoint: "abc", ETag: "def"}},
+	} {
+		if !Equals(k[0], k[1]) {
+			t.Fatalf("Expected %v, got: %v", k[1], k[0])
+		}
+	}
+}
+
+func Test_CDiff_3_ChangedData(t *testing.T) {
+	c1 := Component{Endpoint: "abc", ETag: "def"}
+	c2 := Component{Endpoint: "uvw", ETag: "xyz"}
+	pos1, neg1 := c1.Diff(&c2)
+	pos2, neg2 := c2.Diff(&c1)
+	for _, k := range [][]Supermarketer{
+		{pos1, &Component{Endpoint: "uvw", ETag: "xyz"}},
+		{neg1, &Component{Endpoint: "abc", ETag: "def"}},
+		{pos2, &Component{Endpoint: "abc", ETag: "def"}},
+		{neg2, &Component{Endpoint: "uvw", ETag: "xyz"}},
+	} {
+		if !Equals(k[0], k[1]) {
+			t.Fatalf("Expected %v, got: %v", k[1], k[0])
+		}
+	}
+}
